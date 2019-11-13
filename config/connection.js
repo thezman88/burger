26bd8284297493b7dba62a123b8e1af739a4b6a8
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-11-13 18:46:05 -0500

    jawsdb update

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection(process.env.NODE_ENV==='development'?require('./db-config'):process.env.JAWSDB_URL);
 
+// var connection = mysql.createConnection({   host: "localhost",   port: 3306,   user: "root",   password: "rootroot",   database: "burgers_db" }); 
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
