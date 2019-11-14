65ee382f0a5ba2711f91781567728e0420a18dbd
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-11-13 19:04:54 -0500

    jawsdb working

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection(process.env.NODE_ENV==='development'?require('./db-config'):process.env.JAWSDB_URL);
+// const connection = mysql.createConnection(process.env.NODE_ENV==='development'?require('./db-config'):process.env.JAWSDB_URL);
 
-// var connection = mysql.createConnection({   host: "localhost",   port: 3306,   user: "root",   password: "rootroot",   database: "burgers_db" }); 
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 // Make connection.
 connection.connect(function(err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
